75218bbdff6e9bdfcbf0efdcb50e22ccf1519a6b
Sibi Prabakaran <sibi@psibi.in>
2019-07-03 16:33:02 +0530

    Additional debug

---
+++ tar.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+rm a.txt b.txt c.txt *tar
+
+touch a.txt
+touch b.txt
+touch c.txt
+
+tar -cf a.tar a.txt
+tar -cf b.tar b.txt
+
+echo "View a.tar"
+tar -tvf a.tar
+echo "View b.tar"
+tar -tvf b.tar
+
+tar --force-local -cvf ~/jackal.tar -T /dev/null
+echo "View jackal.tar"
+tar -tvf ~/jackal.tar
+echo "End view" 
+
+rm -rf temp
+mkdir temp
+mv a.tar temp
+tar -C temp --force-local -xf temp/a.tar
+rm temp/a.tar
+tar -C temp --force-local -rf ~/jackal.tar .
+
+
+echo "View jackal.tar"
+tar -tvf ~/jackal.tar
+echo "End view"
